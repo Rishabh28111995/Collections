6a8270a35b51a3bd8d6dae19177a25fb4b40d74e
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-07-07 08:55:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Collections_Demo;
+class Program
+{
+    //Collection is a class that menas we can create an object
+    //can be create or declared instance of a class, so we can add
+    //a data in collections
+    //Collection type : are designed to store, manage similar data
+    //We can used to adding, remove, find,and storing data in collection
+    //Two types : Generic and non-generic collections
+    //Non-generic - arrayList , Hashtable, sortedlist, stack and Queue
+    //Generic - List ,dictionary,Sortedlist,stack and queue
+    static void Main(string[] args)
+    {
+        List_Demo list_Demo = new List_Demo();
+        list_Demo.Adding();
+        Console.ReadLine();
+        
+        
+    }
+}
+
